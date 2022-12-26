00eed573efd30522272e74a7b0b80c75eba32a0e
Ashok Kamath <ashokkamath@Ashoks-MacBook-Pro.local>
2022-12-25 22:40:30 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import logo from './profile_pic.jpeg';
 import './App.css';
-
+import Tabs from "./component/Tabs" ; 
+import SocialFollow from "./SocialFollow";
 function App() {
   return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs>
+        <div label = "Home" id="Home">
+          <h1>
+            Ashok Kamath
+          </h1>
+        </div>
+        <div label="About Me">
+          <h2>About Me</h2>
+          <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Hi, I'm Ashok. Welcome to my website!
+            </p>
+            
+            <h3>Technical Skills</h3>
+              <p>
+                I have worked with a wide variety of programming languages.
+                <ul className="horizontal-list">
+                  <li>Java</li>
+                  <li>Python</li>
+                  <li>JavaScript</li>
+                </ul>
+              </p>
+            <h3>Experience</h3>
+            <h3>Education</h3>
+              <p>
+                I go to Indiana University and expected to graduate in May 2023 
+              </p>
+            <SocialFollow/>
+
+        </div>
+        <div label="Projects">
+        </div>
+        <div label="Blog">
+        </div>
+      </Tabs>
+      <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     </div>
   );
 }
